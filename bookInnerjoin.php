38b05c1a7f68e52446130ca1138da4854f9c77dc
Sencoool <s6506021610037@kmutnb.ac.th>
2024-02-22 15:53:22 +0700

    Coming in hot

--- bookInnerjoin.php
+++ bookInnerjoin.php
@@ -1,5 +1,6 @@
+<!-- SELECT book.BookID, book.BookName, book.StatusID FROM book INNER JOIN statusbook ON book.StatusID = statusbook.StatusID; -->
 <html>
-    <head><title>bookList1.php</title></head>
+    <head><title>bookInnerjoin.php</title></head>
     <body>
         <?php
             $hostname = "localhost";
@@ -15,37 +16,32 @@
             mysqli_query($conn,"set character_set_connection=utf8mb4");
             mysqli_query($conn,"set character_set_client=utf8mb4");
             mysqli_query($conn,"set character_set_results=utf8mb4");
-            $sql = "SELECT * from book order by bookId";
+            $sql = "SELECT book.BookID, book.BookName, book.StatusID FROM book INNER JOIN statusbook ON book.StatusID = statusbook.StatusID";
             $result = mysqli_query ($conn, $sql);
             echo '<center>';
-            echo '<br><h3>รายชื่อหนังสือ</h3>';
+            echo '<br><h3>รายชื่อหนังสือที่มีการเช่ามากกว่า 5 วัน</h3>';
             echo '<table width="500" border="0">';
-            echo '<tr><td align="left"><a href="bookInsert1.php">เพิ่มรายการหนังสือ</a></td><tr>';
             echo '</table>';
             echo '<br><table width="500" border="1">';
             echo '<tr bgcolor="">';
             echo '<th width ="50" >ลำดับ</th>';
             echo '<th width ="100">รหัสหนังสือ</th>';
             echo '<th width ="200">ชื่อหนังสือ</th>';
-            echo '<th width ="80">แก้ไข</th>';
-            echo '<th width ="80">ลบ</th></tr>';
+            echo '<th width ="80">สถานะหนังสือ</th>';
             $row=1;
             while ($rs = mysqli_fetch_array($result))
             {
                 echo '<tr align="center" bgcolor="">';
                 echo '<td>'.$row.'</td>';
-                echo '<td><a href="bookDetail.php?bookId='.$rs[0].'">'.$rs[0].'</a></td>';
-                echo '<td align="left">'.$rs[1].'</td>';
-                echo '<td><a href="bookUpdate1.php?bookId='.$rs[0].'">[แก้ไข]</a></td>';
-                echo '<td><a href="bookDelete1.php?bookId='.$rs[0].'"onclick="return confirm(\' ยืนยันการลบข้อมูลหนังสือ '.$rs[1].'\')">[ลบ]</a></td>';
-
+                echo '<td><a href="bookDetail.php?bookId='.$rs[0].'">'.$rs[0].'</a></td>'; //index 0 is bookID na
+                echo '<td align="left">'.$rs[1].'</td>'; //index 1 is name na
+                echo '<td>'.$rs[2].'</td>'; //index 2 is status na
                 echo '</tr>';
                 $row++;
             }
             echo '</table>';
             mysqli_close ( $conn );
-            echo '<br><br><a href="bookList2.php">หนังสือที่มีการเช่ามากกว่า 5 วัน</a>';
-            echo '<br><br><a href="bookInnerjoin.php">หนังสือที่มีการ Innerjoin ระหว่าง Table book และ Table statusbook</a>';
+            echo '<br><br><a href="bookList1.php">Back to BookList1</a>';
             echo '</center>';
         ?>
     </body>
