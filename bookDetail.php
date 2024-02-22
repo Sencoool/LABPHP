1c95242471d4f49b374ef64282e3c09655d6511b
Sencoool <s6506021610037@kmutnb.ac.th>
2024-02-22 14:56:53 +0700

    Updated Book Detail

--- bookDetail.php
+++ bookDetail.php
@@ -10,13 +10,28 @@
         }
         mysqli_select_db($conn, $dbName) or die("Can't choose database");
         mysqli_query($conn,"set character_set_connection=utf8mb4");
-        mysqli_query($conn,"set character_set_client=utf8mb4");
+        mysqli_query($conn,"set character_set_client=utf8mb4"); // TH Language
         mysqli_query($conn,"set character_set_results=utf8mb4");
         if(isset($_GET['bookId'])) {   // checking if $_get is null or not and then get value book_id from URL
-        $bookIdURL = $_GET['bookId']; // name need 2bt same with Var that declare in PHP
-        $sql = "select * from book where id = $bookIdURL";
-        $result = mysqli_query ($conn, $sql);
-            echo "ค่าของ book_id ที่รับมาคือ: " . $bookIdURL;
+            $bookIdURL = $_GET['bookId']; // name need 2bt same with Var that declare in PHP
+            $sql = "SELECT * FROM book WHERE BookID = $bookIdURL"; // SQL Command needs to be UPPERCASE And BookID here need 2bt same in SQL name
+            $result = mysqli_query ($conn, $sql);
+            if (mysqli_num_rows($result) > 0) {
+                $row = mysqli_fetch_assoc($result);
+                echo "BookID: " . $row["BookID"] . "<br>";
+                echo "BookName: " . $row["BookName"] . "<br>";
+                echo "TypeID : " . $row["TypeID"] . "<br>";
+                echo "StatusID: " . $row["StatusID"] . "<br>";
+                echo "Publish: " . $row["Publish"] . "<br>";
+                echo "UnitPrice: " . $row["UnitPrice"] . "<br>";
+                echo "UnitRent: " . $row["UnitRent"] . "<br>";
+                echo "DayAmount: " . $row["DayAmount"] . "<br>";
+                echo "BookDate: " . $row["BookDate"] . "<br>";
+                echo "PictureURL: " . $row["Picture"] . "<br>";
+                echo "<img src='pictures/{$row['Picture']}' alt='Book Image'><br>";
+            } else {
+                echo "ไม่พบข้อมูลหนังสือ";
+            }
         } else {
             echo "ไม่มีค่าของ book_id ที่รับมา";
         }
