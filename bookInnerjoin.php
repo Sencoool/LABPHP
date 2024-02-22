0631ae633cd63d1ebd4fa76b339ff810ac7ed809
Sencoool <s6506021610037@kmutnb.ac.th>
2024-02-22 15:58:04 +0700

    Updating comment on line 39,40

--- bookInnerjoin.php
+++ bookInnerjoin.php
@@ -16,7 +16,7 @@
             mysqli_query($conn,"set character_set_connection=utf8mb4");
             mysqli_query($conn,"set character_set_client=utf8mb4");
             mysqli_query($conn,"set character_set_results=utf8mb4");
-            $sql = "SELECT book.BookID, book.BookName, book.StatusID FROM book INNER JOIN statusbook ON book.StatusID = statusbook.StatusID"; // Select mean that u want to see the result of this column
+            $sql = "SELECT book.BookID, book.BookName, book.StatusID,statusbook.StatusID FROM book INNER JOIN statusbook ON book.StatusID = statusbook.StatusID"; // Select mean that u want to see the result of this column
             $result = mysqli_query ($conn, $sql);
             echo '<center>';
             echo '<br><h3>การ Innerjoin ระหว่าง Table book และ Table statusbook</h3>';
@@ -27,7 +27,8 @@
             echo '<th width ="50" >ลำดับ</th>';
             echo '<th width ="100">รหัสหนังสือ</th>';
             echo '<th width ="200">ชื่อหนังสือ</th>';
-            echo '<th width ="80">สถานะหนังสือ</th>';
+            echo '<th width ="80">สถานะหนังสือจาก book</th>';
+            echo '<th width ="80">สถานะหนังสือจาก statusbook</th>';
             $row=1;
             while ($rs = mysqli_fetch_array($result))
             {
@@ -35,7 +36,8 @@
                 echo '<td>'.$row.'</td>';
                 echo '<td><a href="bookDetail.php?bookId='.$rs[0].'">'.$rs[0].'</a></td>'; //index 0 is bookID na
                 echo '<td align="left">'.$rs[1].'</td>'; //index 1 is name na
-                echo '<td>'.$rs[2].'</td>'; //index 2 is status na
+                echo '<td>'.$rs[2].'</td>'; //index 2 is status from book na
+                echo '<td>'.$rs[3].'</td>'; //index 3 is status from statusbook na
                 echo '</tr>';
                 $row++;
             }
